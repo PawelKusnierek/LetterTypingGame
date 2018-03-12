3af7f3bbe278126c9673221841628fe402c829fd
Pawel Kusnierek <kusnier@hotmail.co.uk>
2018-03-12 20:44:08 +0000

    setting up script functions

---
+++ script.js
@@ -0,0 +1,48 @@
+var score = 0;
+var screenLetter;
+var pressedLetter;
+var timer;
+
+onLoad();
+
+function onLoad() {
+    // timer = 60;
+    displayNextLetter();
+    // score = 0;
+    //keyListeners and adding keyDown();
+}
+
+function onKeyDown() {
+    //compare screenLetter to pressedLetter
+    //based on outcome either successfulKey or unSuccessfulKey
+}
+
+function successfulKey() {
+    //add 1 to score
+}
+
+function unSuccessfulKey() {
+    //subtract 1 if score > 0
+}
+
+function restartGame() {
+    //resetTimer, nextLetter, resetScore();
+    //reinitiate the listeners
+}
+
+function onTimerEnd() {
+    //stop listeners
+    //display score? Highscores?
+}
+
+function displayNextLetter() {
+    //random letter from predefined array of letters
+}
+
+function resetTimer() {
+
+}
+
+function resetScore() {
+
+}
