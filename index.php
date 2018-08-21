76998293cbd0ea50c8270f2a2a53fdde6e3e72a5
pawel.kusnierek <kusnier@hotmail.co.uk>
2018-08-21 19:43:07 +0100

    heroku database stuff

--- index.php
+++ index.php
@@ -7,10 +7,6 @@ $dbopts = parse_url(getenv('DATABASE_URL'));
 $db_connection = pg_connect("host=".$dbopts["host"]." dbname=".ltrim($dbopts["path"],'/')." user="
         .$dbopts["user"]." password=".$dbopts["pass"]);
 
-//$db_connection = pg_connect("host=ec2-54-247-101-191.eu-west-1.compute.amazonaws.com dbname=d7befugnmmgsln
-//user=mblncxqnoypnkr password=9f086c22a446a448990286faa30c45ff485cf662698c19c9b0b6e6a48e074970");
-
-
 $resultsQuery = file_get_contents('src/queries/top_10_results.sql');
 $result = pg_query($db_connection, $resultsQuery);
 $array = pg_fetch_all($result);
@@ -19,9 +15,6 @@ $array = pg_fetch_all($result);
 <script type="text/javascript">
     let resultsArray = [];
     resultsArray = <?php echo json_encode($array); ?>;
-
-
-
 
     // TODO this is terrible, bad, awful, think of a better way than this shambiotic temporary solution
     const dbResult1 = document.getElementById("dbResult1");
